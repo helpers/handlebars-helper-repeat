cf1fcff75bf9425365d9c51f90bde805a1364c48
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-15 17:16:57 -0400

    use mocha

--- test/test.js
+++ test/test.js
@@ -1,55 +1,75 @@
-/*jshint unused:false*/
-/**
- * {{repeat}}
- * https://github.com/helpers/handlebars-helper-repeat
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT license.
+/*!
+ * handlebars-helper-repeat <https://github.com/helpers/handlebars-helper-repeat>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
 
-// Node.js
-var fs = require('fs');
-var path = require('path');
+/* deps:mocha */
+var assert = require('assert');
+var should = require('should');
+var helper = require('..');
+var Template = require('template');
+var handlebars;
+var template;
 
-// Test utils
-var readFile = function() {
-  var filepath = path.join.apply(null, arguments);
-  return fs.readFileSync(filepath, 'utf8');
-};
-var actual = function(src) {
-  return readFile(path.join(__dirname, 'actual', src));
-};
-var expected = function(src) {
-  return readFile(path.join(__dirname, 'expected', src));
-};
+describe('handlebars', function () {
+  beforeEach(function () {
+    handlebars = require('handlebars');
+    handlebars.registerHelper('repeat', helper(handlebars));
+    handlebars.registerPartial('button', '<button>{{text}}</button>');
+    handlebars.registerPartial('outter', '<button>{{> inner }}</button>');
+    handlebars.registerPartial('inner', '<button>{{zzz}}</button>');
+  });
 
+  it('should repeat a block n times:', function () {
+    var ctx = {text: 'foo'};
+    var actual = handlebars.compile('{{#repeat 2}}{{> button }}\n{{/repeat}}')(ctx);
+    actual.should.eql('<button>foo</button>\n<button>foo</button>\n');
+  });
 
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
+  it('should output the inverse when no number is specified:', function () {
+    var ctx = {text: 'foo'};
+    var actual = handlebars.compile('{{#repeat}}{{> button }}{{else}}Nothing :({{/repeat}}')(ctx);
+    actual.should.eql('Nothing :(');
+  });
 
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
+  it('should allow private variables to be passed on the context:', function () {
+    var ctx = {opts: {repeat: {whatever: 'Bar'}}};
+    var actual = handlebars.compile('{{#repeat 1 opts}}{{@whatever}}{{/repeat}}')(ctx);
+    actual.should.eql('<button>Bar</button>\n');
+  });
 
-exports['repeat'] = {
-  repeat: function(test) {
-    test.expect(2);
-    test.equal(actual('index.html'), expected('index.html'), 'Should be equal');
-    test.equal(actual('repeat.html'), expected('repeat.html'), 'Should be equal');
-    test.done();
-  }
-};
+  // it('should allow context to be passed:', function () {
+  //   var ctx = {a: {b: {c: {text: 'DDD'}}}};
+  //   handlebars.compile('{{repeat "button" a.b.c}}')(ctx).should.eql('Click me!DDDafter');
+  // });
+
+  // it('should work with other repeats:', function () {
+  //   var ctx = {zzz: ' __ZZZ__ '};
+  //   handlebars.compile('{{repeat "outter"}}')(ctx).should.eql('Click me!Click me! __ZZZ__ afterafter');
+  // });
+});
+
+// describe('Template', function () {
+//   beforeEach(function () {
+//     template = new Template();
+//     handlebars = require('engine-handlebars');
+
+//     template.engine('hbs', handlebars);
+//     template.helper('repeat', helper(handlebars));
+
+//     template.repeat('button.hbs', 'Click me!{{text}}after');
+//     template.data({text: ' __BAR__ '})
+//   });
+
+//   it('should work with Template:', function (done) {
+//     template.render('{{repeat "button.hbs"}}', {ext: 'hbs'}, function (err, content) {
+//       if (err) console.log(err)
+//       content.should.eql('Click me! __BAR__ after');
+//     });
+//     done();
+//   });
+// });
