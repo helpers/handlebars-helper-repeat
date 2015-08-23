cc213428d19293f9aa5d6f5315e653b85bf508cf
Adam Ratcliffe <adam.ratcliffe@gmail.com>
2015-08-24 11:53:51 +1200

    Add first and last variables

--- index.js
+++ index.js
@@ -48,7 +48,9 @@ module.exports = function repeat(n, options) {
     hash.start = hash.start || 0;
 
     for (var i = hash.start; i < n + hash.start; i++) {
-      hash.index = i;
+        hash.index = i;
+        hash.first = (i == 0);
+        hash.last = (i == n-1);
 
       str += opts.fn(opts, {data: hash});
     }
